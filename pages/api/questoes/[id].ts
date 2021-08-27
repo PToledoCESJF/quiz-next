d05ac4b9d30af77455c1a6def8ef91239ace47e1
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-08-27 13:43:31 -0300

    Implementando questoes e bancoDeQuestoes

--- pages/api/questoes/[id].ts
+++ pages/api/questoes/[id].ts
@@ -1,5 +1,14 @@
+import questoes from "../bancoDeQuestoes";
+
 export default function handler(req, res) {
-    res.status(200).json({
-        id: +req.query.id,
-        name: 'John Doe' })
+    const idSelecionado = +req.query.id
+
+    const unicaQuestaoOuNada = questoes.filter(questao => questao.id === idSelecionado)
+
+    if(unicaQuestaoOuNada.length === 1 ){
+        const questaoSelecionada = unicaQuestaoOuNada[0]
+        res.status(200).json(questaoSelecionada.toObjeto())
+    } else {
+        res.status(204).send()
+    }
   }
