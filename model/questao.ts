b91337104666741d9604e4af3d53cb299b430fd4
Paulo Toledo <ptoledo.bsices@gmail.com>
2021-08-27 13:42:59 -0300

    Implementando o metodo toObjeto

--- model/questao.ts
+++ model/questao.ts
@@ -2,39 +2,48 @@ import RespostaModel from "./resposta"
 
 export default class QuestaoModel {
     #id: number
-    #enunciado:string
+    #enunciado: string
     #respostas: RespostaModel[]
     #acertou: boolean
-    
 
-    constructor(id: number, enunciado:string, respostas: RespostaModel[], acertou = false){
+
+    constructor(id: number, enunciado: string, respostas: RespostaModel[], acertou = false) {
         this.#id = id
         this.#enunciado = enunciado
         this.#respostas = respostas
         this.#acertou = acertou
     }
 
-    get id(){
+    get id() {
         return this.#id
     }
 
-    get enunciado(){
+    get enunciado() {
         return this.#enunciado
     }
 
-    get respostas(){
+    get respostas() {
         return this.#respostas
     }
 
-    get acertou(){
+    get acertou() {
         return this.#acertou
     }
 
-    get respondida(){
-        for(let resposta of this.#respostas){
-            if(resposta.revelada) return true
+    get respondida() {
+        for (let resposta of this.#respostas) {
+            if (resposta.revelada) return true
         }
         return false
+    }
+
+    toObjeto() {
+        return {
+            id: this.#id,
+            enunciado: this.#enunciado,
+            respostas: this.#respostas.map(resp => resp.toObjeto()),
+            acertou: this.#acertou,
+        }
     }
 
 }
